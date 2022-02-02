97fea186dc30e1add5a47d0531b03cb98e57a08a
RezzedUp <RezzedUp@users.noreply.github.com>
2022-02-02 15:30:07 -0600

    Clean up economy hooks by utilizing OOP.

---
+++ src/main/java/me/justeli/coins/hook/TreasuryEconomyHook.java
@@ -0,0 +1,101 @@
+package me.justeli.coins.hook;
+
+import me.justeli.coins.hook.treasury.EconomySubscribers;
+import me.lokka30.treasury.api.economy.EconomyProvider;
+import me.lokka30.treasury.api.economy.account.PlayerAccount;
+import me.lokka30.treasury.api.economy.transaction.EconomyTransactionInitiator;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
+
+/** by Rezz on February 02, 2022 **/
+final class TreasuryEconomyHook implements EconomyHook
+{
+    public static final String TREASURY = "Treasury";
+    
+    private final EconomyProvider economy;
+    
+    TreasuryEconomyHook (EconomyProvider economy)
+    {
+        this.economy = economy;
+    }
+    
+    @Override
+    public void balance (UUID uuid, DoubleConsumer balance)
+    {
+        economy.retrievePlayerAccount(
+            uuid,
+            EconomySubscribers.requesting(PlayerAccount.class)
+                .success(playerAccount ->
+                    playerAccount.retrieveBalance(
+                        economy.getPrimaryCurrency(),
+                        EconomySubscribers.requesting(BigDecimal.class)
+                            .success(bigDecimal -> balance.accept(bigDecimal.doubleValue()))
+                            .silentFailure()
+                    )
+                )
+                .silentFailure()
+        );
+    }
+    
+    @Override
+    public void canAfford (UUID uuid, double amount, Consumer<Boolean> canAfford)
+    {
+        economy.retrievePlayerAccount(
+            uuid,
+            EconomySubscribers.requesting(PlayerAccount.class)
+                .success(playerAccount ->
+                    playerAccount.canAfford(
+                        BigDecimal.valueOf(amount),
+                        economy.getPrimaryCurrency(),
+                        EconomySubscribers.requesting(Boolean.class)
+                            .success(canAfford)
+                            .silentFailure()
+                    )
+                )
+                .silentFailure()
+        );
+    }
+    
+    @Override
+    public void withdraw (UUID uuid, double amount, Runnable success)
+    {
+         economy.retrievePlayerAccount(
+            uuid,
+            EconomySubscribers.requesting(PlayerAccount.class)
+                .success(playerAccount ->
+                    playerAccount.withdrawBalance(
+                        BigDecimal.valueOf(amount),
+                        EconomyTransactionInitiator.SERVER,
+                        economy.getPrimaryCurrency(),
+                        EconomySubscribers.requesting(BigDecimal.class)
+                            .success(bigDecimal -> success.run())
+                            .silentFailure()
+                    )
+                )
+                .silentFailure()
+        );
+    }
+    
+    @Override
+    public void deposit (UUID uuid, double amount, Runnable success)
+    {
+        economy.retrievePlayerAccount(
+            uuid,
+            EconomySubscribers.requesting(PlayerAccount.class)
+                .success(playerAccount ->
+                    playerAccount.depositBalance(
+                        BigDecimal.valueOf(amount),
+                        EconomyTransactionInitiator.SERVER,
+                        economy.getPrimaryCurrency(),
+                        EconomySubscribers.requesting(BigDecimal.class)
+                            .success(bigDecimal -> success.run())
+                            .silentFailure()
+                    )
+                )
+                .silentFailure()
+        );
+    }
+}
